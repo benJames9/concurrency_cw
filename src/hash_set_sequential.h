9f4375f8ebe8a42fef1329cf4e6449964cc99fbf
sa2021 <sa2021@ic.ac.uk>
2023-10-30 15:21:37 +0000

    Implemented sequential hash set

--- src/hash_set_sequential.h
+++ src/hash_set_sequential.h
@@ -1,34 +1,66 @@
 #ifndef HASH_SET_SEQUENTIAL_H
 #define HASH_SET_SEQUENTIAL_H
 
+#include <algorithm>
 #include <cassert>
+#include <functional>
+#include <vector>
 
 #include "src/hash_set_base.h"
 
-template <typename T>
-class HashSetSequential : public HashSetBase<T> {
+template <typename T> class HashSetSequential : public HashSetBase<T> {
  public:
-  explicit HashSetSequential(size_t /*initial_capacity*/) {}
+  explicit HashSetSequential(size_t initial_capacity)
+      : set_size_(0), table_(initial_capacity) {}
 
-  bool Add(T /*elem*/) final {
-    assert(false && "Not implemented yet");
-    return false;
+  bool Add(T elem) final {
+    if (Contains(elem)) {
+      return false;
+    }
+
+    // Add element to correct bucket from hash value
+    size_t bucket_index = std::hash<T>()(elem) % table_.size();
+    table_[bucket_index].push_back(elem);
+    set_size_++;
+
+    // Double number of buckets if resize policy is satisfied.
+    if (Policy()) {
+      table_.resize(table_.size() * 2);
+    }
+    return true;
   }
 
-  bool Remove(T /*elem*/) final {
-    assert(false && "Not implemented yet");
-    return false;
+  bool Remove(T elem) final {
+    if (!Contains(elem)) {
+      return false;
+    }
+    
+    // Remove element from correct bucket (based on hash value)
+    size_t bucket_index = std::hash<T>()(elem) % table_.size();
+    std::vector<T> bucket = table_[bucket_index];
+    bucket.erase(std::remove(bucket.begin(), bucket.end(), elem), bucket.end());
+    set_size_--;
+    return true;
   }
 
-  [[nodiscard]] bool Contains(T /*elem*/) final {
-    assert(false && "Not implemented yet");
-    return false;
+  // Looks up element in correct bucket (based on hash value)
+  [[nodiscard]] bool Contains(T elem) final {
+    size_t bucket_index = std::hash<T>()(elem) % table_.size();
+    std::vector<T> bucket = table_[bucket_index];
+    return std::find(bucket.begin(), bucket.end(), elem) != bucket.end();
   }
 
-  [[nodiscard]] size_t Size() const final {
-    assert(false && "Not implemented yet");
-    return 0u;
+  [[nodiscard]] size_t Size() const final { return set_size_; }
+
+ private:
+  const size_t bucket_capacity_ = 4;
+  size_t set_size_;
+  std::vector<std::vector<T>> table_;
+
+  // Returns true if average bucket size exceeds bucket_capacity_
+  bool Policy() {
+    return set_size_ / table_.size() > bucket_capacity_;
   }
 };
 
-#endif  // HASH_SET_SEQUENTIAL_H
+#endif // HASH_SET_SEQUENTIAL_H
