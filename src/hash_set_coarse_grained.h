1e94e1a45fce8de8b72cd922073e7d2232465eeb
Ben <bj420@ic.ac.uk>
2023-11-01 18:05:43 +0000

    Fixes seg fault with removing from bucket

--- src/hash_set_coarse_grained.h
+++ src/hash_set_coarse_grained.h
@@ -16,11 +16,11 @@ class HashSetCoarseGrained : public HashSetBase<T> {
         : set_size_(0), table_(initial_capacity) {}
 
   bool Add(T elem) final {
+    std::scoped_lock<std::mutex> lock(w_mutex_);
+
     if (Contains(elem)) {
       return false;
     }
-
-    std::scoped_lock<std::mutex> lock(mutex_);
 
     // Add element to correct bucket from hash value
     size_t bucket_index = std::hash<T>()(elem) % table_.size();
@@ -42,32 +42,25 @@ class HashSetCoarseGrained : public HashSetBase<T> {
   }
 
   bool Remove(T elem) final {
+    std::scoped_lock<std::mutex> lock(w_mutex_);
+
     if (!Contains(elem)) {
       return false;
     }
-
-    std::scoped_lock<std::mutex> lock(mutex_);
     
     // Remove element from correct bucket (based on hash value)
     size_t bucket_index = std::hash<T>()(elem) % table_.size();
     std::vector<T> bucket = table_[bucket_index];
-    // bucket.erase(std::remove(bucket.begin(), bucket.end(), elem), bucket.end());
-    table_[bucket_index].erase(std::remove(bucket.begin(), bucket.end(), elem), bucket.end());
+    table_[bucket_index].erase(std::remove(table_[bucket_index].begin(), 
+        table_[bucket_index].end(), elem), table_[bucket_index].end());
     set_size_.fetch_sub(1);
     
-    // std::cout << "\ntable contents:\n";
-    // for (auto b : table_) {
-    //     for (T item : b) {
-    //         std::cout << item << "\n";
-    //     }
-    // }
-
     return true;
   }
 
   // Looks up element in correct bucket (based on hash value)
   [[nodiscard]] bool Contains(T elem) final {
-    std::scoped_lock<std::mutex> lock(mutex_);
+    std::scoped_lock<std::mutex> lock(r_mutex_);
     size_t bucket_index = std::hash<T>()(elem) % table_.size();
     std::vector<T> bucket = table_[bucket_index];
     return std::find(bucket.begin(), bucket.end(), elem) != bucket.end();
@@ -82,7 +75,8 @@ class HashSetCoarseGrained : public HashSetBase<T> {
   const size_t bucket_capacity_ = 4;
   std::atomic<size_t> set_size_;
   std::vector<std::vector<T>> table_;
-  std::mutex mutex_; 
+  std::mutex w_mutex_; 
+  std::mutex r_mutex_;
 
   // Returns true if average bucket size exceeds bucket_capacity_
   bool Policy() {
