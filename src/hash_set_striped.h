2dfd2f721ffa859f03278361a4b2673579eda0b6
Wenxing Zheng <wxz21@texel26.doc.ic.ac.uk>
2023-11-02 18:48:11 +0000

    Fixed deadlock in striped resize

--- src/hash_set_striped.h
+++ src/hash_set_striped.h
@@ -13,7 +13,7 @@ template <typename T>
 class HashSetStriped : public HashSetBase<T> {
  public:
   explicit HashSetStriped(size_t initial_capacity)
-        : set_size_(0), table_(initial_capacity), mutexes_(initial_capacity) {}
+        : set_size_(0), capacity_(initial_capacity), table_(initial_capacity), mutexes_(initial_capacity) {}
 
   bool Add(T elem) final {
     Acquire(elem);
@@ -24,15 +24,17 @@ class HashSetStriped : public HashSetBase<T> {
     }
 
     // Add element to correct bucket from hash value
-    size_t bucket_index = std::hash<T>()(elem) % table_.size();
+    size_t bucket_index = std::hash<T>()(elem) % capacity_.load();
     table_[bucket_index].push_back(elem);
     set_size_.fetch_add(1);
-    Release(elem);
 
     // Double number of buckets if resize policy is satisfied.
     if (Policy()) {
+	    Release(elem);
       Resize();
-    }
+    } else {
+	    Release(elem);
+		}
 
     return true;
   }
@@ -46,7 +48,7 @@ class HashSetStriped : public HashSetBase<T> {
     }
     
     // Remove element from correct bucket (based on hash value)
-    size_t bucket_index = std::hash<T>()(elem) % table_.size();
+    size_t bucket_index = std::hash<T>()(elem) % capacity_.load();
     std::vector<T>& bucket = table_[bucket_index];
     bucket.erase(std::remove(bucket.begin(), bucket.end(), elem), bucket.end());
     // Decrements size atomically
@@ -71,19 +73,28 @@ class HashSetStriped : public HashSetBase<T> {
  private:
   const size_t bucket_capacity_ = 4;
   std::atomic<size_t> set_size_;
+	std::atomic<size_t> capacity_; // number of buckets
   std::vector<std::vector<T>> table_;
   mutable std::vector<std::mutex> mutexes_;
  
 
   // Returns true if average bucket size exceeds bucket_capacity_
   bool Policy() {
-	  return set_size_ / table_.size() > bucket_capacity_;
+	  return set_size_ / capacity_.load() > bucket_capacity_;
   }
 
   // Perform a resizing, re-hashing all elements
   void Resize() { 
-    AcquireAll();
-    size_t new_size = table_.size() * 2;
+		size_t old_size = capacity_.load();
+		AcquireAll();
+
+		// Return if thread has already updated the capacity 
+		if (old_size != capacity_.load()) {
+			ReleaseAll();
+			return;
+		}
+
+    size_t new_size = old_size * 2;
     std::vector<std::vector<T>> new_table(new_size);
 
     // Copy elems to new table
@@ -93,6 +104,7 @@ class HashSetStriped : public HashSetBase<T> {
           new_table[new_bucket_index].push_back(elem);
         }
     } 
+		capacity_.store(new_size);
 
     // Explicitly clear old table 
     table_.clear();
@@ -101,7 +113,7 @@ class HashSetStriped : public HashSetBase<T> {
   }
 
   bool Contains_(T elem) {
-    size_t bucket_index = std::hash<T>()(elem) % table_.size();
+    size_t bucket_index = std::hash<T>()(elem) % capacity_.load();
     std::vector<T>& bucket = table_[bucket_index];
     return std::find(bucket.begin(), bucket.end(), elem) != bucket.end();
   }
@@ -121,8 +133,8 @@ class HashSetStriped : public HashSetBase<T> {
   }
 
   void ReleaseAll() {
-    for (size_t i = 0; i < mutexes_.size(); i++) {
-      mutexes_[i].unlock();
+    for (size_t i = 1; i <= mutexes_.size(); i++) {
+      mutexes_[mutexes_.size() - i].unlock();
     }
   }
 };
